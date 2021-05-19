52801828d159dfdce285084f09ccf64ebb4e52a8
Maycon Carlete <maycon.carlete@hotmail.com>
2021-05-19 20:26:04 -0300

    feat: good header example

---
+++ mod-02/header-example.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#
+# header-example.sh
+#
+# Author: Maycon Carlete
+# Email: maycon.carlete@gmail.com
+# Maintener: Maycon Carlete
+#
+#-------------------------------------------#
+# Description of script
+# ...
+#
+#   Example:
+#       ./header-example.sh
+#
+#--------------------------------------------#
+#
+# History of changes
+#   v1.0 01/01/2020
+#       - Changelog
+#   v1.1 01/02/2020
+#       -Changelog
+#
+#--------------------------------------------#
+#   Tested in bash version:
+#   bash 5.0.17
+# 
+#--------------------------------------------#
+#   Supporters and Thanks
+#
+#--------------------------------------------#
+#
+#   Variables
+#
+#--------------------------------------------#
+#
+#   Tests
+#
+#--------------------------------------------#
+#
+#   Functions
+#
+#--------------------------------------------#
+#
+#   Execution
+    echo "Hello world path: $0"
+#--------------------------------------------#
